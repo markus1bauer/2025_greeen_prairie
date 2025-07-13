4e70d95b17624ab6d10bc25855c45200c0a454c8
markus1bauer <markus1.bauer@tum.de>
2025-07-13 08:05:54 -0400

    Update show_figure_1_map.R

--- R/show_figure_1_map.R
+++ R/show_figure_1_map.R
@@ -1,10 +1,10 @@
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# GRASSWORKS Project
+# GREEEN Prairie Project
 # Show figure map ####
 # Map of the study sites
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Markus Bauer
-# 2025-05-20
+# 2025-07-13
 
 
 
@@ -41,31 +41,16 @@ theme_mb <- function() {
 
 ### Load data ###
 
-sites <- st_read(
-  here("data", "raw", "data_processed_sites_epsg4326.gpkg")
-  ) %>%
-  filter(
-    eco.id %in% c(664, 654, 686) & !(eco.id == 686 & region == "centre")
-    )
+# sites <- st_read(
+#   here("data", "raw", "data_processed_sites_epsg4326.gpkg")
+#   ) %>%
+#   st_transform(crs = 4326)
 
 ecoregions <- st_read(here("data", "raw", "ecoregions2017.shp")) %>%
-  st_transform(crs = 4326) %>%
-  filter(
-    ECO_NAME == "Western European broadleaf forests" |
-      ECO_NAME == "Central European mixed forests" |
-      ECO_NAME == "European Atlantic mixed forests" |
-      ECO_NAME == "Baltic mixed forests" |
-      ECO_NAME == "Alps conifer and mixed forests"
-  )
-
-germany <- geodata::gadm(
-  country = "DEU", level = 0, version = "latest", resolution = 2, path = here()
-) %>%
-  st_as_sf() %>%
-  st_set_crs(4326)
+  st_transform(crs = 4326)
 
 elevation <- elevatr::get_elev_raster(
-  locations = data.frame(x = c(5.5, 15.5), y = c(47, 55.5)),
+  locations = data.frame(x = c(-85, 75), y = c(40, 48)),
   prj = "+proj=longlat +datum=WGS84 +no_defs",
   clip = "bbox", z = 5
 ) %>%
@@ -74,7 +59,7 @@ elevation <- elevatr::get_elev_raster(
 rivers <- rnaturalearth::ne_download(
   scale = 50, type = 'rivers_lake_centerlines', category = 'physical'
 ) %>%
-  st_crop(xmin = 5.5, xmax = 15.5, ymin = 47, ymax = 55.4)
+  st_crop(xmin = -85, xmax = -75, ymin = 40, ymax = 48)
 
 
 
@@ -84,8 +69,7 @@ rivers <- rnaturalearth::ne_download(
 
 
 
-clip <- st_intersection(ecoregions, germany)
-crop <- st_crop(ecoregions, xmin = 5.5, xmax = 15.5, ymin = 47, ymax = 55.4)
+crop <- st_crop(ecoregions, xmin = -85, xmax = -75, ymin = 40, ymax = 48)
 mask <- mask(elevation, crop)
 gradient_1 <- tidyterra::hypso.colors2(10, "dem_poster")
 
@@ -102,25 +86,24 @@ gradient_1 <- tidyterra::hypso.colors2(10, "dem_poster")
 
 graph_sites <- ggplot() +
   geom_sf(
-    data = clip, colour = "black", fill = "transparent", size = 1,
+    data = crop, colour = "black", fill = "transparent", size = 1,
     linetype = "dashed"
   ) +
-  geom_sf(data = germany, colour = "black", fill = "transparent", size = 1) +
-  geom_sf(data = sites, colour = "black", size = 1) +
-  annotate(
-    geom = "label", x = c(9.9, 12.85, 11), y = c(52.8, 51.55, 48.7),
-    label = c("ID 664", "ID 654", "ID 686"), fill = "transparent", size = 3.5
-  ) +
-  annotate(
-    geom = "text", x = c(8, 13, 13.8, 10), y = c(53.1, 53.85, 52.2, 50),
-    lineheight = .8, size = 3.5,
-    label = c(
-      "European Atlantic\nmixed forests",
-      "Baltic\nmixed forests",
-      "Central European\nmixed forests",
-      "Western European\nbroadleaf forests"
-      )
-  ) +
+  # geom_sf(data = sites, colour = "black", size = 1) +
+  # annotate(
+  #   geom = "label", x = c(9.9, 12.85, 11), y = c(52.8, 51.55, 48.7),
+  #   label = c("ID 664", "ID 654", "ID 686"), fill = "transparent", size = 3.5
+  # ) +
+  # annotate(
+  #   geom = "text", x = c(8, 13, 13.8, 10), y = c(53.1, 53.85, 52.2, 50),
+  #   lineheight = .8, size = 3.5,
+  #   label = c(
+  #     "European Atlantic\nmixed forests",
+  #     "Baltic\nmixed forests",
+  #     "Central European\nmixed forests",
+  #     "Western European\nbroadleaf forests"
+  #     )
+  # ) +
   ggspatial::annotation_north_arrow(
     which_north = "true",
     style = ggspatial::north_arrow_fancy_orienteering(),
@@ -155,26 +138,26 @@ graph_sites <- ggplot() +
     linetype = "solid"
     ) +
   geom_sf(data = rivers, color = "blue", linewidth = 0.2) +
-  annotate(
-    geom = "text", label = c("Danube", "Elbe"),
-    x = c(13.5, 11.7), y = c(48.95, 53.25),
-    angle = 333, color = "blue", size = 3.5
-  ) +
-  geom_sf(data = sites, colour = "black", size = 1) +
-  annotate(
-    geom = "label", x = c(9.85, 14, 11), y = c(52.85, 51.7, 49.2),
-    label = c("ID 664", "ID 654", "ID 686"), fill = "transparent", size = 3.5,
-  ) +
-  annotate(
-    geom = "text", x = c(7.6, 13, 13.4, 10.4), y = c(53, 53.82, 52.2, 49.7),
-    lineheight = .8, size = 3.5,
-    label = c(
-      "European Atlantic\nmixed forests",
-      "Baltic\nmixed forests",
-      "Central European\nmixed forests",
-      "Western European\nbroadleaf forests"
-    )
-  ) +
+  # annotate(
+  #   geom = "text", label = c("Danube", "Elbe"),
+  #   x = c(13.5, 11.7), y = c(48.95, 53.25),
+  #   angle = 333, color = "blue", size = 3.5
+  # ) +
+  # geom_sf(data = sites, colour = "black", size = 1) +
+  # annotate(
+  #   geom = "label", x = c(9.85, 14, 11), y = c(52.85, 51.7, 49.2),
+  #   label = c("ID 664", "ID 654", "ID 686"), fill = "transparent", size = 3.5,
+  # ) +
+  # annotate(
+  #   geom = "text", x = c(7.6, 13, 13.4, 10.4), y = c(53, 53.82, 52.2, 49.7),
+  #   lineheight = .8, size = 3.5,
+  #   label = c(
+  #     "European Atlantic\nmixed forests",
+  #     "Baltic\nmixed forests",
+  #     "Central European\nmixed forests",
+  #     "Western European\nbroadleaf forests"
+  #   )
+  # ) +
   ggspatial::annotation_north_arrow(
     which_north = "true",
     style = ggspatial::north_arrow_fancy_orienteering(),
