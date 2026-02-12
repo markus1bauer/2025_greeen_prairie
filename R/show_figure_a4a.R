080783bdc34097c64654ccf9415127ce9ec63dd9
markus1bauer <markus1.bauer@tum.de>
2026-02-12 18:09:06 +0100

    create new figure A4

---
+++ R/show_figure_a4a.R
@@ -0,0 +1,108 @@
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# GREEEN prairie project
+# Site Lux Arbor
+# Show figure ####
+# Establishment rate ~ pool size * seeding time
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Markus Bauer
+# 2026-02-12
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# A Preparation ###############################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+### Packages ###
+library(here)
+library(tidyverse)
+library(ggeffects)
+library(ggbeeswarm)
+
+### Start ###
+rm(list = setdiff(ls(), c("graph_a", "graph_b", "graph_c", "graph_d", "m")))
+
+### Functions ###
+theme_mb <- function() {
+  theme(
+    panel.background = element_rect(fill = "white"),
+    text = element_text(size = 9, color = "black"),
+    strip.text = element_text(size = 10),
+    axis.text = element_text(angle = 0, hjust = 0.5, size = 9,
+                             color = "black"),
+    axis.title = element_text(angle = 0, hjust = 0.5, size = 9,
+                              color = "black"),
+    axis.line = element_line(),
+    legend.key = element_rect(fill = "white"),
+    legend.position = "bottom",
+    legend.text = element_text(size = 9),
+    legend.margin = margin(0, 0, 0, 0, "cm"),
+    plot.margin = margin(0, 0, 0, 0, "cm"),
+    plot.title = element_text(hjust = 0.5, face = "bold")
+  )
+}
+
+#### Load data ###
+data <- read_csv(
+  here("data", "processed", "data_processed_sites.csv"),
+  col_names = TRUE, na = c("na", "NA", ""), col_types = cols(
+    .default = "?",
+    id_plot_year = "f",
+    id_plot = "f",
+    site = col_factor(
+      levels = c("NW Station", "Lux Arbor", "SW Station"), ordered = FALSE
+    ),
+    year = "f",
+    seeding_time = col_factor(
+      levels = c("unseeded", "fall", "spring"), ordered = FALSE
+    ),
+    herbicide = col_factor(levels = c("0", "1"), ordered = FALSE),
+    seeded_pool = col_factor(
+      levels = c("0", "6", "12", "18", "33"), ordered = TRUE
+    ),
+    treatment_id = "f",
+    treatment_description = "c",
+    richness_type = "f"
+  )
+) %>%
+  filter(
+    site == "Lux Arbor",
+    richness_type == "seeded_richness",
+    !(treatment_id %in% c("2", "4"))
+  ) %>%
+  mutate(y = establishment_rate)
+
+
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# B Plot ######################################################################
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+
+(graph_a <- ggplot(
+  data = data,
+  aes(x = seeded_pool, y = y, fill = seeding_time)
+  ) +
+    geom_boxplot() +
+    facet_grid(~year) +
+    scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, .2)) +
+    scale_fill_manual(
+      breaks = c("fall", "spring"),
+      labels = c("Autumn", "Spring"),
+      values = c("#440154", "#FFA500")
+    ) +
+    labs(
+      x = "Seeded species pool [#]", fill = "Planting season",
+      y = expression(Establishment ~ rate),
+      title = "Lux Arbor"
+      ) +
+    theme_mb())
+
+### Save ###
+ggsave(
+  here("outputs", "figures", "figure_a4a_300dpi_16x5cm.tiff"),
+  dpi = 300, width = 16, height = 5, units = "cm"
+  )
